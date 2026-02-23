ccb019fb5911a9ce82c7be52fd63337ae73aab6e
Tiago Oliveira Silva <tiago.silva@minu.co>
2026-02-23 18:13:20 -0300

    ajuste de porta

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ dotenv.config({ path: path.resolve(__dirname, 'envs', '.env') });
 // 2. Se não achar o .env (como no CI/CD), usa o .env.example como fallback de segurança
 dotenv.config({ path: path.resolve(__dirname, 'envs', '.env.example'), override: false });
 
-const apiBaseUrl = process.env.API_GOVERNO_URL ?? "http://127.0.0.1:8080";
+const apiBaseUrl = process.env.API_GOVERNO_URL ?? "http://127.0.0.1:8081";
 const frontendBaseUrl = process.env.FRONTEND_URL ?? "http://localhost:3000";
 
 export default defineConfig({
